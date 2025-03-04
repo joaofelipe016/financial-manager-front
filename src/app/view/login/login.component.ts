f2df83cd0acd45e9afb71b58842e2b1fa6e72f79
joaofelipe016 <61212164+joaofelipe016@users.noreply.github.com>
2025-03-04 08:48:18 -0300

    Arrumando minha tela de login

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
-import { ControlContainer, FormBuilder, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import {
+  ControlContainer,
+  FormBuilder,
+  FormGroup,
+  FormGroupDirective,
+  Validators,
+} from '@angular/forms';
 import { ModalService } from 'src/app/services/modal.service';
 import { CadastroComponent } from '../cadastro/cadastro.component';
-import { faGithub, faGoogle, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import {
+  faGithub,
+  faGoogle,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faEnvelope,
+  faIdCard,
+  faLock,
+} from '@fortawesome/free-solid-svg-icons';
 import { AuthController } from 'src/app/controllers/auth.controller';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -15,20 +30,24 @@ import { Router } from '@angular/router';
   viewProviders: [
     {
       provide: ControlContainer,
-      useExisting: FormGroupDirective
-    }
-  ]
+      useExisting: FormGroupDirective,
+    },
+  ],
 })
-export class LoginComponent {
-  @ViewChild('modalContainer', { read: ViewContainerRef, static: true }) modalContainer!: ViewContainerRef;
-
+export class LoginComponent implements OnInit {
+  iconsLoaded = false;
+  @ViewChild('modalContainer', { read: ViewContainerRef, static: true })
+  modalContainer!: ViewContainerRef;
+  isRightPanelActive = false;
   formGroup: FormGroup;
+  signUpForm: FormGroup;
   faGithub = faGithub;
   faGoogle = faGoogle;
   faLinkedin = faLinkedin;
   faEnvelope = faEnvelope;
   faLock = faLock;
-
+  faUser = faUser;
+  faIdCard = faIdCard;
   constructor(
     private formBuilder: FormBuilder,
     private modalService: ModalService,
@@ -37,29 +56,53 @@ export class LoginComponent {
   ) {
     this.formGroup = this.formBuilder.group({
       scUsuario: ['', [Validators.required]],
-      scSenha: ['', [Validators.required]]
+      scSenha: ['', [Validators.required]],
+    });
+
+    this.signUpForm = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      cpf: ['', [Validators.required]],
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
     });
   }
-
   ngOnInit(): void {
     this.modalService.setViewContainerRef(this.modalContainer);
+    this.initializeIcons();
   }
-
-  openModal(): void{
+  private initializeIcons(): void {
+    Promise.resolve().then(() => {
+      this.faGoogle = faGoogle;
+      this.faGithub = faGithub;
+      this.faLinkedin = faLinkedin;
+      this.faUser = faUser;
+      this.faIdCard = faIdCard;
+      this.faLock = faLock;
+      this.faEnvelope = faEnvelope;
+      this.iconsLoaded = true;
+    });
+  }
+  togglePanel(): void {
+    this.isRightPanelActive = !this.isRightPanelActive;
+  }
+  openModal(): void {
     this.modalService.open(CadastroComponent, {
       title: 'Modal Title',
-      content: 'Modal Message'
+      content: 'Modal Message',
     });
   }
 
   onSubmit(): void {
-    this.authService.login(this.formGroup.value).then((response) => {
-      if(response.accessToken != null) {
-        this.router.navigate(['/home'])
-      }
-      console.log('Login realizado com sucesso');
-    }).catch(() => {
-      console.error('Erro ao fazer login');
-    });
+    this.authService
+      .login(this.formGroup.value)
+      .then((response) => {
+        if (response.accessToken != null) {
+          this.router.navigate(['/home']);
+        }
+        console.log('Login realizado com sucesso');
+      })
+      .catch(() => {
+        console.error('Erro ao fazer login');
+      });
   }
 }
